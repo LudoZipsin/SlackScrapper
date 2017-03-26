c321ab8d632ef05e3f461103432d5e7beb04046d
Ludovic Zipsin <ludovic.j.r.zipsin@gmail.com>
2017-03-26 23:34:27 +0200

    Removing beautifulsoup and requests dependecies to packtpub since robobrowser wraps them.

--- Actionner/PacktPubActionner.py
+++ Actionner/PacktPubActionner.py
@@ -1,9 +1,6 @@
 import os
 
 from robobrowser import RoboBrowser
-from bs4 import BeautifulSoup
-import requests
-import sys
 
 from .Actionner import Actionner
 
@@ -36,28 +33,26 @@ class PacktPubActionner(Actionner):
 
         browser.submit_form(login_form)
 
+        url_link = None
+        opener = browser.find(class_="twelve-days-claim")
+        if opener is not None:
+            url_link = opener.attrs["href"]
+
+        if url_link is None:
+            return {"status": "fail", "reason": "Unable to get the free ebook url."}
+
         browser.open(URL_DOMAIN + url_book_list)
         last_ebook_title = browser.find(class_="product-line").attrs["title"]
 
         browser.open(URL_DOMAIN + url_free_learning)
         dotd_title = browser.find(class_="dotd-title").h2.text.strip()
 
-        response = requests.get(URL_DOMAIN + url_free_learning, headers={'User-Agent': CUSTOM_USER_AGENT})
-        soup = BeautifulSoup(response.text, "html.parser")
-
-        url_link = None
-
-        for link in soup.find_all('a'):
-            if pattern_free_ebook in str(link.get('href')):
-                url_link = link.get('href')
-                browser.open(URL_DOMAIN + url_link)
-                new_ebook_title = browser.find(class_="product-line").attrs["title"]
-                message = "The ebook '" + new_ebook_title + "' has been donwloaded."
-                if last_ebook_title == new_ebook_title:
-                    message = "You already had this ebook: " + dotd_title + "."
-                return {"status": "success", "content": message}
-        if url_link is None:
-            return {"status": "fail", "reason": "Unable to get the free ebook url."}
+        browser.open(URL_DOMAIN + url_link)
+        new_ebook_title = browser.find(class_="product-line").attrs["title"]
+        message = "The ebook '" + new_ebook_title + "' has been donwloaded."
+        if last_ebook_title == new_ebook_title:
+            message = "You already had this ebook: " + dotd_title + "."
+        return {"status": "success", "content": message}
 
 
 
