49eb412ca9893d692edfac6bd19ef0f3f10372b5
Ludovic Zipsin <ludovic.j.r.zipsin@gmail.com>
2017-03-26 20:50:42 +0200

    A little more verbose when trying to fetch a new ebook. And typos correction

--- Actionner/PacktPubActionner.py
+++ Actionner/PacktPubActionner.py
@@ -22,6 +22,7 @@ class PacktPubActionner(Actionner):
         self.password = os.environ.get(ENV_PASS)
 
     def fetch_free_learning(self) -> dict:
+        url_book_list = "/account/my-ebooks"
         url_free_learning = "/packt/offers/free-learning"
         pattern_free_ebook = "/freelearning-claim/"
         form_id = "packt-user-login-form"
@@ -35,14 +36,26 @@ class PacktPubActionner(Actionner):
 
         browser.submit_form(login_form)
 
+        browser.open(URL_DOMAIN + url_book_list)
+        last_ebook_title = browser.find(class_="product-line").attrs["title"]
+
+        browser.open(URL_DOMAIN + url_free_learning)
+        dotd_title = browser.find(class_="dotd-title").h2.text.strip()
+
         response = requests.get(URL_DOMAIN + url_free_learning, headers={'User-Agent': CUSTOM_USER_AGENT})
         soup = BeautifulSoup(response.text, "html.parser")
+
         url_link = None
+
         for link in soup.find_all('a'):
             if pattern_free_ebook in str(link.get('href')):
                 url_link = link.get('href')
                 browser.open(URL_DOMAIN + url_link)
-                return {"status": "success", "content": "One ebook as been downloaded."}
+                new_ebook_title = browser.find(class_="product-line").attrs["title"]
+                message = "The ebook '" + new_ebook_title + "' has been donwloaded."
+                if last_ebook_title == new_ebook_title:
+                    message = "You already had this ebook: " + dotd_title + "."
+                return {"status": "success", "content": message}
         if url_link is None:
             return {"status": "fail", "reason": "Unable to get the free ebook url."}
 
